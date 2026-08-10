d1f28b9b34e49b35718cde4635dcdaa2e793f145
Sk Ismail <skismailkhan256@gmail.com>
2026-08-10 23:06:46 +0530

    Added function practice in python

---
+++ function_basics.py
@@ -0,0 +1,21 @@
+# def greet(fname, lname,bass):
+#     print("Good morining! ",fname, lname,bass)
+#     print("How are you! ")
+#     print("Thank you! ")
+    
+# greet("Ismail", "Khan" ,"bass")
+# greet("madhu", "Begam", "bass")
+
+# def add(a, b):
+#     return a + b
+
+# a, b = map(int, input("Enter two numbers: ").split())
+
+# c = add(a, b)
+# print(c)
+
+def greet(name ="user", city="Delhi"):
+    print("hello", name, city)
+greet()
+greet("Ismail")
+greet(city="Alwar", name="Madhu")
