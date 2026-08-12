82fcfaa8b4bf2c59349c9968fdcb7dbf14b79bc2
Sk Ismail <skismailkhan256@gmail.com>
2026-08-12 23:12:18 +0530

    Added loop basic

---
+++ loop_basics.py
@@ -0,0 +1,16 @@
+# items=["apples", 34, "bananas", "oranges"]
+
+# for item in items:
+#     print(item)
+# for i in range (0, 10 , ):
+#     print(i)
+# for char in "Harry":
+#     print(char)
+
+
+count = 1
+
+while (count <= 5):
+    print(count)
+    count +=1
+print("End of program")
